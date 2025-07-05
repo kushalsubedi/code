8b0beeef53428f7de8e2be75f13f3854ba38eda3
kushalsubedi <kushalsubedi55@gmail.com>
2025-07-05 19:36:18 +0545

    feat!: this should work

--- .github/scripts/check_commit.sh
+++ .github/scripts/check_commit.sh
@@ -4,7 +4,7 @@ set -e
 COMMIT_MSG=$(git log -1 --pretty=format:"%s" | tr -d '\r' | sed 's/^[ \t]*//;s/[ \t]*$//')
 echo "Commit: \"$COMMIT_MSG\""
 
-REGEX='^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)(\([a-z0-9-_]+\))?(!)?: .+|^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)!: .+'
+REGEX='^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)(\([a-z0-9-_]+\))?!?: .+'
 
 if [[ "$COMMIT_MSG" =~ ^Merge\ [a-f0-9]{7,}\ into\ [a-f0-9]{7,}$ ]]; then
   echo "Skipping merge commit."
