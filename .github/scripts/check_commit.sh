9fd7f1b638f6bdba4a1b4f210d31a42521b3f678
kushalsubedi <kushalsubedi55@gmail.com>
2025-07-05 19:34:20 +0545

    feat!: this should work

--- .github/scripts/check_commit.sh
+++ .github/scripts/check_commit.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 set -e
 
-# Grab the latest commit message
 COMMIT_MSG=$(git log -1 --pretty=format:"%s" | tr -d '\r' | sed 's/^[ \t]*//;s/[ \t]*$//')
-
-#COMMIT_MSG=$(git log -1 --pretty=format:"%s")
 echo "Commit: \"$COMMIT_MSG\""
 
-# Conventional commit pattern
-#REGEX='^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)(\([a-z0-9-_]+\))?(!)?: .+|^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)!: .+'
-REGEX='^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)(!|\([a-z0-9-_]+\)|\([a-z0-9-_]+\)!){0,1}: .+'
+REGEX='^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)(\([a-z0-9-_]+\))?(!)?: .+|^(feat|fix|chore|docs|style|refactor|perf|test|build|ci|revert|breaking)!: .+'
 
-# Ignore merge commits
 if [[ "$COMMIT_MSG" =~ ^Merge\ [a-f0-9]{7,}\ into\ [a-f0-9]{7,}$ ]]; then
   echo "Skipping merge commit."
   exit 0
 fi
 
-# Check if the commit messages are from above regex or not
 if [[ ! "$COMMIT_MSG" =~ $REGEX ]]; then
   echo "::error ::Commit doesn't follow conventional format."
   echo "Example: feat(auth): add login endpoint"
   exit 1
 fi
 
-echo "Conventional Commit Check Pass"
+echo "Conventional Commit Check Passed"
 
